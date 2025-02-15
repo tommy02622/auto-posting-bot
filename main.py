4b4227ea23facce33c6001c9707afe46ceb7df1e
TOP_GUN <k02622@naver.com>
2025-02-16 02:14:48 +0900

    Update project

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import tweepy
-import schedule
 import time
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
 def authenticate_twitter_app():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    return tweepy.API(auth)
-
-def post_tweet(api, message):
     try:
-        api.update_status(message)
-        print("Successfully posted tweet!")
+        auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+        print("Authentication successful")
+        return api
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Authentication error: {e}")
 
-def job():
-    api = authenticate_twitter_app()
-    message = "Hello, this is an automated tweet!"
-    post_tweet(api, message)
+def upload_media(api, media_path):
+    try:
+        media = api.media_upload(media_path)
+        print(f"Media uploaded successfully: {media.media_id}")
+        return media.media_id
+    except Exception as e:
+        print(f"Media upload error: {e}")
+        return None
 
-schedule.every().day.at("10:00").do(job)
+def post_tweet_with_media(api, message, media_path):
+    media_id = upload_media(api, media_path)
+    if media_id:
+        try:
+            api.update_status(status=message, media_ids=[media_id])
+            print("Successfully posted tweet with media!")
+        except Exception as e:
+            print(f"Error: {e}")
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+api = authenticate_twitter_app()
+post_tweet_with_media(api, "이미지 테스트 트윗!", r"C:\Users\gunhu\Desktop\KickboardDetectionProject\images\1.jpg")
+
